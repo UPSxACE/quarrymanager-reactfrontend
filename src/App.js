ba1dc6eb6783f2f8182a08270c733ed7d29f6a3a
Eduardo Botelho de Castro Pinto <a45480@alunos.ipb.pt>
2022-05-16 22:29:30 +0100

    criacao da pagina equipa

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Routes, Route, Link } from "react-router-dom";
+import { Equipa } from "./website/pages/equipa/equipa";
 
 // App.js
 function Home() {
@@ -34,10 +35,11 @@ function About() {
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to React Router!</h1>
+      {/*<h1>Welcome to React Router!</h1>*/}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
+        <Route path="equipa" element={<Equipa />} />
       </Routes>
     </div>
   );
---
+++ src/website/pages/equipa/equipa.js
@@ -0,0 +1,9 @@
+export { Equipa };
+
+function Equipa() {
+  //estados
+  //funções
+  //return
+
+  return <h1>Equipa</h1>;
+}
