59279df84b725468c04def02055b960cbff07a49
Monique Maria de Oliveira Santos <a52177@alunos.ipb.pt>
2022-05-18 14:46:02 +0100

    Reestruturação do codigo da pagina Home

--- src/website/pages/home/home.js
+++ src/website/pages/home/home.js
@@ -15,9 +15,49 @@ function Home() {
           <MainPicture src={mainPicture}></MainPicture>
         </Col>
       </Row>
-      <SloganRow></SloganRow>
-      <TextImageRow></TextImageRow>
-      <TextImageRow2></TextImageRow2>
+      <StyledRowSlogan>
+        <Col xs={6}>
+          <DisplayH1>
+            <strong>weDELIVER</strong>
+            <H1>Somos nós que entregamos</H1>
+          </DisplayH1>
+        </Col>
+        <Col xs={6}>
+          <DisplayH1>
+            <strong>weDELIVER</strong>
+            <H1>Garantimos a qualidade do serviço</H1>
+          </DisplayH1>
+        </Col>
+      </StyledRowSlogan>
+      <StyledRowText1>
+        <Col xs={4}>
+          <H2>PRODUTO SEMPRE COM A MELHOR QUALIDADE!</H2>
+          <Paragraph>
+            A extracção de pedra natural é o que a weROCK faz há mais de 50
+            anos, com diversas pedreiras a nivél nacional. Fazemos de tudo para
+            oferecer sempre aos nossos clientes rochas ornamentais de
+            reconhecida qualidade e uma elevada capacidade de produção para
+            grandes projectos.
+          </Paragraph>
+        </Col>
+        <Col xs={8}>
+          <IndexPic1 src={indexPic1}></IndexPic1>
+        </Col>
+      </StyledRowText1>
+      <StyledRowText2>
+        <Col xs={8}>
+          <IndexPic2 src={indexPic2}></IndexPic2>
+        </Col>
+        <Col xs={4}>
+          <H2>RESPONSABILIDADE E DETERMINAÇÃO</H2>
+          <Paragraph>
+            Dominamos a arte de arrancar pedra, acumulamos uma experiência
+            inestimável e temos sempre a mesma energia e a motivação, desde o
+            dia em que a empresa abriu , para trazermos aos nossos clientes as
+            melhores extrações que pode encontrar em Portugal.
+          </Paragraph>
+        </Col>
+      </StyledRowText2>
     </Container>
   );
 }
@@ -28,52 +68,14 @@ const MainPicture = styled.img`
   object-fit: cover;
 `;
 
-const SloganRow = styled(SloganContainer)`
+const StyledRowSlogan = styled(Row)`
   background-color: #394a58;
 `;
 
-function SloganContainer(props) {
-  return (
-    <Row className={props.className}>
-      <Col xs={6}>
-        <DisplayH1>
-          <strong>weDELIVER</strong>
-          <H1>Somos nós que entregamos</H1>
-        </DisplayH1>
-      </Col>
-      <Col xs={6}>
-        <DisplayH1>
-          <strong>weDELIVER</strong>
-          <H1>Garantimos a qualidade do serviço</H1>
-        </DisplayH1>
-      </Col>
-    </Row>
-  );
-}
-
-const TextImageRow = styled(TextImageRowContainer)`
+const StyledRowText1 = styled(Row)`
   background-color: #30373e;
   height: 425px;
 `;
-
-function TextImageRowContainer(props) {
-  return (
-    <Row className={props.className}>
-      <Col xs={4}>
-        <H2>PRODUTO SEMPRE COM A MELHOR QUALIDADE!</H2>
-        <Paragraph>
-          A extracção de pedra natural é o que a weROCK faz há mais de 50 anos,
-          com diversas pedreiras a nivél nacional. Fazemos de tudo para oferecer
-          sempre aos nossos clientes rochas ornamentais de reconhecida qualidade
-          e uma elevada capacidade de produção para grandes projectos.
-        </Paragraph>
-      </Col>
-      <Col xs={8}>
-        <IndexPic1 src={indexPic1}></IndexPic1>
-      </Col>
-    </Row>
-  );
-}
 
 const IndexPic1 = styled.img`
   height: 425px;
@@ -81,29 +83,10 @@ const IndexPic1 = styled.img`
   object-fit: cover;
 `;
 
-const TextImageRow2 = styled(TextImageRowContainer2)`
+const StyledRowText2 = styled(Row)`
   background-color: #30373e;
   height: 425px;
 `;
-
-function TextImageRowContainer2(props) {
-  return (
-    <Row className={props.className}>
-      <Col xs={8}>
-        <IndexPic2 src={indexPic2}></IndexPic2>
-      </Col>
-      <Col xs={4}>
-        <H2>RESPONSABILIDADE E DETERMINAÇÃO</H2>
-        <Paragraph>
-          Dominamos a arte de arrancar pedra, acumulamos uma experiência
-          inestimável e temos sempre a mesma energia e a motivação, desde o dia
-          em que a empresa abriu , para trazermos aos nossos clientes as
-          melhores extrações que pode encontrar em Portugal.
-        </Paragraph>
-      </Col>
-    </Row>
-  );
-}
 
 const IndexPic2 = styled.img`
   height: 425px;
