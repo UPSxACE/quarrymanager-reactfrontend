7e8970855112fa1985f334076ea6a1cedd29bc63
Eduardo Botelho de Castro Pinto <a45480@alunos.ipb.pt>
2022-05-19 17:50:14 +0100

    footer lists

--- src/website/components/footer.js
+++ src/website/components/footer.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import { Container, Row, Col } from "react-bootstrap";
 import wRLogo from "../../img/website/wR.png";
 import { Paragraph } from "./text";
-import { H2 } from "./text";
+import { H1 } from "./text";
 
 export { Footer };
 
@@ -27,14 +27,73 @@ function Footer() {
                 <Col xs={3}>
                   <TightList>
                     <ListHeader>
-                      <H2>Serviços</H2>
+                      <ListHeaderTitle>Serviços</ListHeaderTitle>
                       <WhiteLine />
+                      <FooterList>
+                        <FooterListClickable href="#">
+                          <FooterListItem>Parceiros</FooterListItem>
+                        </FooterListClickable>
+                        <FooterListClickable href="#">
+                          <FooterListItem>Loja</FooterListItem>
+                        </FooterListClickable>
+                      </FooterList>
                     </ListHeader>
                   </TightList>
                 </Col>
-                <Col xs={3}></Col>
-                <Col xs={3}></Col>
-                <Col xs={3}></Col>
+                <Col xs={3}>
+                  <TightList>
+                    <ListHeader>
+                      <ListHeaderTitle>Acerca de</ListHeaderTitle>
+                      <WhiteLine />
+                      <FooterList>
+                        <FooterListClickable href="#">
+                          <FooterListItem>Equipa</FooterListItem>
+                        </FooterListClickable>
+                        <FooterListClickable href="#">
+                          <FooterListItem>Criadores</FooterListItem>
+                        </FooterListClickable>
+                        <FooterListClickable href="#">
+                          <FooterListItem>Pedreiras</FooterListItem>
+                        </FooterListClickable>
+                      </FooterList>
+                    </ListHeader>
+                  </TightList>
+                </Col>
+                <Col xs={3}>
+                  <TightList>
+                    <ListHeader>
+                      <ListHeaderTitle>Segue-nos</ListHeaderTitle>
+                      <WhiteLine />
+                      <FooterList>
+                        <FooterListClickable href="#">
+                          <FooterListItem>Facebook</FooterListItem>
+                        </FooterListClickable>
+                        <FooterListClickable href="#">
+                          <FooterListItem>Instagram</FooterListItem>
+                        </FooterListClickable>
+                        <FooterListClickable href="#">
+                          <FooterListItem>Twitter</FooterListItem>
+                        </FooterListClickable>
+                      </FooterList>
+                    </ListHeader>
+                  </TightList>
+                </Col>
+                <Col xs={3}>
+                  <TightList>
+                    <ListHeader>
+                      <ListHeaderTitle>Ajuda</ListHeaderTitle>
+                      <WhiteLine />
+                      <FooterList>
+                        <FooterListClickable href="#">
+                          <FooterListItem>Contacte-nos</FooterListItem>
+                        </FooterListClickable>
+                        <FooterListClickable href="#">
+                          <FooterListItem>FAQ</FooterListItem>
+                        </FooterListClickable>
+                      </FooterList>
+                    </ListHeader>
+                  </TightList>
+                </Col>
               </Row>
             </Container>
           </Col>
@@ -100,4 +159,21 @@ const WhiteLine = styled.span`
   font-size: 2px;
   padding-left: 100%;
   background-color: white;
+`;
+
+const ListHeaderTitle = styled(H1)`
+  line-height: 16px;
+`;
+
+const FooterList = styled.ul`
+  list-style: none;
+  padding: 0px;
+`;
+
+const FooterListClickable = styled.a`
+  text-decoration: none;
+`;
+
+const FooterListItem = styled.li`
+  color: white;
 `;
