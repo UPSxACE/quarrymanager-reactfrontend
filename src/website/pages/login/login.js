f393a92b4ce4719164ab3411a671e6f7f9728a9c
Monique Maria de Oliveira Santos <a52177@alunos.ipb.pt>
2022-05-23 20:33:29 +0100

    Correção de cores formulário Login

--- src/website/pages/login/login.js
+++ src/website/pages/login/login.js
@@ -15,7 +15,7 @@ export { Login };
 function Login() {
   return (
     <Container fluid>
-      <ColoredRow variant={3}>
+      <ColoredRow variant={1}>
         <CardBodyRow className={"container-fluid mt-5 mb-5 p-5"}>
           <Col xs={12} className={"g-0"}>
             <H3 className={"text-center"}>LOGIN</H3>
@@ -47,11 +47,13 @@ function Login() {
                 <Row>
                   <Col xs={6}>
                     <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                      <Form.Check type="checkbox" label="Lembra-me" />
+                      <StyledText type="checkbox" label="Lembra-me" />
                     </Form.Group>
                   </Col>
                   <Col xs={6} className={"text-end"}>
-                    <a href="#">Esqueceu-se da senha?</a>
+                    <ForgotPassText href="#">
+                      Esqueceu-se da senha?
+                    </ForgotPassText>
                   </Col>
                   <Col xs={12} className={"d-flex justify-content-center"}>
                     <LoginButtonSubmit>Login</LoginButtonSubmit>
@@ -69,4 +71,22 @@ function Login() {
 const CardBodyRow = styled.div`
   background-color: #5e666d;
   width: 600px;
+  border-radius: 17px;
+  filter: drop-shadow(0 4px 4px rgba(0, 0, 0, 25%));
+`;
+
+const StyledText = styled(Form.Check)`
+  color: white;
+
+  &:focus {
+    border-color: #f3844f !important;
+  }
+`;
+
+const ForgotPassText = styled.a`
+  color: white;
+
+  &:hover {
+    color: #f3844f;
+  }
 `;
