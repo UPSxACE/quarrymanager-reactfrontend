5b6b30489bd1084ea591a0df9a137eca77812d37
Monique Maria de Oliveira Santos <a52177@alunos.ipb.pt>
2022-05-26 15:39:21 +0100

    fix

--- src/website/pages/faq/faq.js
+++ src/website/pages/faq/faq.js
@@ -69,7 +69,7 @@ function Faq() {
                   irure dolor in reprehenderit in voluptate velit esse cillum
                   dolore eu fugiat nulla pariatur. Excepteur sint occaecat
                   cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum. >
+                  mollit anim id est laborum.
                 </Paragraph>
               </AccordionBody>
             </AccordionItem>
