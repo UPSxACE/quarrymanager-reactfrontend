4ae5d469c9239f8f1e3cdcf5c631b3b983c828ed
Eduardo Botelho de Castro Pinto <a45480@alunos.ipb.pt>
2022-05-28 15:33:52 +0100

    implementação de routing com multiplos caminhos raizes, utilizando o componente Outlet do react router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, Outlet } from "react-router-dom";
 import { Equipa } from "./website/pages/equipa/equipa";
 import { Home } from "./website/pages/home/home";
 import { Parceiros } from "./website/pages/parceiros/parceiros";
@@ -16,6 +16,7 @@ import { Login } from "./website/pages/login/login";
 import { Register } from "./website/pages/register/register";
 
 import { Faq } from "./website/pages/faq/faq";
+import { DashboardHome } from "./dashboard/pages/home/home";
 
 // App.js
 
@@ -33,26 +34,49 @@ function About() {
   );
 }
 
-function App() {
+function Website() {
+  return (
+    <div className="Website">
+      <NavbarComponent isGuest={true} />
+      <Outlet />
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div className="Dashboard">
+      <h1>Funciona!</h1>
+      <Outlet />
+    </div>
+  );
+}
+
+function App(props) {
   return (
     <div className="App">
-      <NavbarComponent isGuest={true} />
-
+      {/*<NavbarComponent isGuest={true} />*/}
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="equipa" element={<Equipa />} />
-        <Route path="home" element={<Home />} />
-        <Route path="parceiros" element={<Parceiros />} />
-        <Route path="pedreiras" element={<Pedreiras />} />
-        <Route path="criadores" element={<Criadores />} />
-        <Route path="contactos" element={<Contactos />} />
-        <Route path="login" element={<Login />} />
-        <Route path="index" element={<Home />} />
+        {/*<Route path="/" element={<Home />} />*/}
+        <Route path="/" element={<Website />}>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="equipa" element={<Equipa />} />
+          <Route path="home" element={<Home />} />
+          <Route path="parceiros" element={<Parceiros />} />
+          <Route path="pedreiras" element={<Pedreiras />} />
+          <Route path="criadores" element={<Criadores />} />
+          <Route path="contactos" element={<Contactos />} />
+          <Route path="login" element={<Login />} />
+          <Route path="index" element={<Home />} />
+          <Route path="register" element={<Register />} />
+          <Route path="faq" element={<Faq />} />
+        </Route>
 
-        <Route path="register" element={<Register />} />
-
-        <Route path="faq" element={<Faq />} />
+        <Route path="dashboard" element={<Dashboard />}>
+          <Route path="/dashboard/" element={<DashboardHome />} />
+          <Route path="home" element={<DashboardHome />} />
+        </Route>
       </Routes>
       <Footer />
     </div>
---
+++ src/dashboard/pages/home/home.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import {} from "react-bootstrap";
+import styled from "styled-components";
+export { DashboardHome };
+
+function DashboardHome() {
+  return (
+    <div>
+      <h1>Isto é o componente DashboardHome</h1>
+    </div>
+  );
+}
