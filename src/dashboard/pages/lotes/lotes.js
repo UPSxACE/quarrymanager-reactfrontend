1c691a5e708f4cda521fbeb4ab3244e5251cc719
Eduardo Botelho de Castro Pinto <a45480@alunos.ipb.pt>
2022-05-31 19:11:33 +0100

    serviço rest lote

--- src/dashboard/pages/lotes/lotes.js
+++ src/dashboard/pages/lotes/lotes.js
@@ -8,12 +8,37 @@ export { DashboardLotes };
 
 //teste
 function DashboardLotes() {
-  const [data, setData] = useState({ hits: [] });
+  const [data, setData] = useState([]);
 
   useEffect(() => {
+    const axios = require("axios");
+
+    const sendGetRequest = async () => {
+      try {
+        const username = "dC9VOjlGLSmsg6ZGkh7E0DJKz8G1K59O";
+        const password = "";
+
+        const resp = await axios.get("http://localhost:8080/api/lote", {
+          headers: {
+            Authorization: "Basic " + btoa(username + ":" + password),
+            //+ btoa("dC9VOjlGLSmsg6ZGkh7E0DJKz8G1K59O") ,
+          },
+        });
+        //setData(resp.data);
+        setData(resp.data);
+      } catch (err) {
+        // Handle Error Here
+        console.error(err);
+      }
+    };
+
+    sendGetRequest();
+
+    /*
     const config = {
       headers: {
-        Authorization: "Basic dC9VOjlGLSmsg6ZGkh7E0DJKz8G1K59O",
+        Authorization: "Basic ZEM5Vk9qbEdMU21zZzZaR2toN0UwREpLejhHMUs1OU86",
+        //+ btoa("dC9VOjlGLSmsg6ZGkh7E0DJKz8G1K59O") ,
       },
     };
 
@@ -28,29 +53,31 @@ function DashboardLotes() {
 
       setData(result.data);
     }
-    fetchData();
+    fetchData();*/
   }, []);
 
   return (
     <DashboardLayout>
       <h1>Isto é a página de lotes</h1>
       <ul>
-        {data.hits.map((lote) => (
-          <li key={lote.codigo_lote}>
-            <h2>
-              <strong>Código Lote:</strong>
-              {lote.codigo_lote}
-            </h2>
-            <h3>
-              <strong>Quantidade:</strong>
-              {lote.quantidade}
-            </h3>
-            <h3>
-              <strong>Data/Hora:</strong>
-              {lote.dataHora}
-            </h3>
-          </li>
-        ))}
+        {data.map((lote) => {
+          return (
+            <li key={lote.codigo_lote}>
+              <h2>
+                <strong>Código Lote:</strong>
+                {lote.codigo_lote}
+              </h2>
+              <h3>
+                <strong>Quantidade:</strong>
+                {lote.quantidade}
+              </h3>
+              <h3>
+                <strong>Data/Hora:</strong>
+                {lote.dataHora}
+              </h3>
+            </li>
+          );
+        })}
       </ul>
     </DashboardLayout>
   );
