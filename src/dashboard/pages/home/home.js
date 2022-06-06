b87e77244a18be379da3aa4c9c002a390d31499c
Eduardo Botelho de Castro Pinto <a45480@alunos.ipb.pt>
2022-06-06 12:52:12 +0100

    grafico donut

--- src/dashboard/pages/home/home.js
+++ src/dashboard/pages/home/home.js
@@ -2,18 +2,27 @@ import React, { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import styled from "styled-components";
 import { DashboardLayout } from "../../components/layout";
-import { Bar } from "react-chartjs-2";
+import { Bar, Doughnut, Pie } from "react-chartjs-2";
 import { Chart as ChartJS } from "chart.js/auto";
 
 export { DashboardHome };
 
 function DashboardHome() {
   const labels = ["janeiro", "fevereiro", "março", "abril"];
-  const options = {
+  const options1 = {
     responsive: true,
     maintainAspectRatio: false,
+    plugins: { legend: { display: false } },
   };
-  const data = {
+  const options2 = {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      legend: { position: "bottom", labels: { boxWidth: 30, boxHeight: 30 } },
+    },
+    datasets: { doughnut: { cutout: "65%" } },
+  };
+  const data1 = {
     labels,
     datasets: [
       {
@@ -28,6 +37,23 @@ function DashboardHome() {
       },
     ],
   };
+  const data2 = {
+    labels,
+    datasets: [
+      {
+        label: "Dataset 1",
+        data: [1, 2, 3, 4],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+      },
+    ],
+  };
 
   return (
     <DashboardLayout>
@@ -37,10 +63,13 @@ function DashboardHome() {
             <h1>Isto é a home</h1>
             <div className="d-flex">
               <DashboardGraph className="left50">
-                <BarChart chartData={data} chartOptions={options}></BarChart>
+                <BarChart chartData={data1} chartOptions={options1}></BarChart>
               </DashboardGraph>
               <DashboardGraph className="right50">
-                <BarChart chartData={data} chartOptions={options}></BarChart>
+                <DoughnutChart
+                  chartData={data2}
+                  chartOptions={options2}
+                ></DoughnutChart>
               </DashboardGraph>
             </div>
           </Col>{" "}
@@ -52,6 +81,14 @@ function DashboardHome() {
 
 function BarChart({ chartData, chartOptions }) {
   return <Bar data={chartData} options={chartOptions}></Bar>;
+}
+
+function PieChart({ chartData, chartOptions }) {
+  return <Pie data={chartData} options={chartOptions}></Pie>;
+}
+
+function DoughnutChart({ chartData, chartOptions }) {
+  return <Doughnut data={chartData} options={chartOptions}></Doughnut>;
 }
 
 const DashboardGraph = styled.div`
