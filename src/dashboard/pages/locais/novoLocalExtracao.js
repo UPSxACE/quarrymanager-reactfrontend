33a341c998b6b96a14643c6beec30560b906f244
Monique Maria de Oliveira Santos <a52177@alunos.ipb.pt>
2022-06-08 18:49:35 +0100

    Componente Buttons e Styles

--- src/dashboard/pages/locais/novoLocalExtracao.js
+++ src/dashboard/pages/locais/novoLocalExtracao.js
@@ -8,6 +8,10 @@ import {
 } from "../../components/layoutComponents";
 
 import { H1, H2 } from "../../components/layoutComponents";
+import {
+  PrimaryButtonSave,
+  SecundaryButtonCancel,
+} from "../../components/buttons";
 export { DashboardNovoLocalExtracao };
 
 //teste
@@ -16,24 +20,30 @@ function DashboardNovoLocalExtracao() {
     <DashboardLayout>
       <Container fluid>
         <Form>
-          <DashboardRow className="g-0 pt-4 pb-4">
-            <Col xs={6} className="ps-5 pe-4">
-              <H2>Novo Local de Extração</H2>
-              <Form.Label>Nome</Form.Label>
+          <DashboardRow className="g-0 pt-4 pb-4 pt-4 ps-5 pe-5">
+            <Col xs={12}>
+              <H2 className="pb-3">Novo Local de Extração</H2>
+              <FormColor>Nome</FormColor>
               <Form.Group className="mb-3" controlId="formBasicIdLote">
                 <Form.Control type="text" placeholder="" />
               </Form.Group>
-              <Form.Label>coordenadas GPS X</Form.Label>
+            </Col>
+            <Col xs={6} className="pe-3">
+              <FormColor>Coordenadas GPS X</FormColor>
               <Form.Group className="mb-3" controlId="formBasicIdLote">
                 <Form.Control type="text" placeholder="" />
               </Form.Group>
-              <Form.Label>coordenadas GPS Y</Form.Label>
+            </Col>
+            <Col xs={6}>
+              <FormColor>Coordenadas GPS Y</FormColor>
               <Form.Group className="mb-3" controlId="formBasicIdLote">
                 <Form.Control type="text" placeholder="" />
               </Form.Group>
-              <Button variant="success">Enviar</Button>
+            </Col>
+            <Col xs={12} className="pt-3">
+              <PrimaryButtonSave className="me-2">Enviar</PrimaryButtonSave>
 
-              <Button variant="danger">Cancelar</Button>
+              <SecundaryButtonCancel>Cancelar</SecundaryButtonCancel>
             </Col>
           </DashboardRow>
         </Form>
@@ -41,3 +51,7 @@ function DashboardNovoLocalExtracao() {
     </DashboardLayout>
   );
 }
+
+const FormColor = styled(Form.Label)`
+  color: #004b5b;
+`;
