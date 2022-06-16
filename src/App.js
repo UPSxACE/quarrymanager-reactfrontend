ac5fd0c429e1565c628c98b3b04f597f82cf5c6d
Diogo Jose Branquinho Carvalho <a44765@alunos.ipb.pt>
2022-06-16 19:04:38 +0100

    correcoes

--- src/App.js
+++ src/App.js
@@ -48,6 +48,7 @@ import {
 } from "./dashboard/pages/utilizadores/editarUtilizadores";
 import { EditarCor } from "./dashboard/pages/lotes/editarCor";
 import { EditarMaterial } from "./dashboard/pages/lotes/editarMaterial";
+import { ViewProduto } from "./dashboard/pages/lotes/viewProduto";
 
 // App.js
 
@@ -143,8 +144,8 @@ function App(props) {
             path="editar-local-extracao"
             element={<EditarLocalExtracao />}
           />
-          <Route path="editar-produto" element={<EditarProduto />} />
-
+          <Route path="editar-produto/:id" element={<EditarProduto />} />
+          <Route path="ver-produto/:id" element={<ViewProduto />} />
           <Route path="editar-stock" element={<EditarStock />} />
 
           <Route path="editar-lote" element={<EditarLote />} />
