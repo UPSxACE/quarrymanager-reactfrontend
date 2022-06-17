5f65d67ad59140d921ba6c83934a45f56ec66779
Monique Maria de Oliveira Santos <a52177@alunos.ipb.pt>
2022-06-17 22:30:00 +0100

    feature/Produto.js close #37
    done

--- src/dashboard/pages/locais/locais.js
+++ src/dashboard/pages/locais/locais.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import styled from "styled-components";
 import { DashboardLayout } from "../../components/layout";
