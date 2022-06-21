249e1fe3ecfbc9eac5e01f517f4b5e00ef30ba79
Diogo Jose Branquinho Carvalho <a44765@alunos.ipb.pt>
2022-06-21 18:46:01 +0100

    ver encomendas

--- src/dashboard/pages/encomendas/encomendas.js
+++ src/dashboard/pages/encomendas/encomendas.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import styled from "styled-components";
 import { DashboardLayout } from "../../components/layout";
 import axios from "axios";
@@ -8,6 +8,7 @@ import {
   DashboardMenuList,
   DashboardMenuListItem,
   DashboardMenuListItemRight,
+  DashboardRow,
   H3,
 } from "../../components/layoutComponents";
 import { DashboardTable, TablePager } from "../../components/dashboardTable";
@@ -59,5 +60,133 @@ function DashboardEncomendas() {
 }
 
 function DashboardVerEncomendas() {
-  return <></>;
+  return (
+    <Container fluid>
+      <ButtonsRow>
+        <Col xs={12}>
+          <Button variant="secondary"> Dados da Encomenda </Button>
+          <Button variant="secondary"> Mobilização do Stock</Button>
+          <Button variant="secondary"> Agendar Recolha</Button>
+        </Col>
+      </ButtonsRow>
+      <EncomendaWrapper className="g-0 pt-4 pb-4 ps-5 pe-5">
+        <Col xs={12}>
+          <Container fluid>
+            <Row>
+              <BoxCol xs={6}>
+                <h1>O Status: </h1>
+              </BoxCol>
+              <Col xs={6}>ad</Col>
+            </Row>
+            <Row>
+              <Col xs={6} className="g-0">
+                <Button variant="secondary"> Dados da Encomenda </Button>
+                <BoxContainer className="p-5">
+                  <Row>
+                    <Col xs={6}>
+                      <div>
+                        <h6>Nome:</h6>
+                        <span>abc</span>
+                      </div>
+                    </Col>
+                    <Col xs={6}>
+                      {" "}
+                      <div>
+                        <h6>Morada:</h6>
+                        <span>abc</span>
+                      </div>
+                    </Col>
+                    <Col xs={6}>
+                      {" "}
+                      <div>
+                        <h6>Email:</h6>
+                        <span>abc@abc</span>
+                      </div>
+                    </Col>
+                    <Col xs={6}>
+                      {" "}
+                      <div>
+                        <h6>Telefone:</h6>
+                        <span>99999999</span>
+                      </div>
+                    </Col>
+                    <Col xs={12}>
+                      Detalhes da Conta do Utilizador
+                      <textarea className="w-100"></textarea>
+                    </Col>
+                  </Row>
+                </BoxContainer>
+              </Col>
+              <Col xs={6} className="g-0">
+                <Button variant="secondary"> Dados da Encomenda </Button>
+                <BoxContainer className="pb-5 pe-5">
+                  <Row>
+                    <Col xs={6}>
+                      <div>
+                        <h6>Produto:</h6>
+                        <span>abc</span>
+                      </div>
+                    </Col>
+                    <Col xs={6}>
+                      {" "}
+                      <div>
+                        <h6>Preço/m²:</h6>
+                        <span>abc</span>
+                      </div>
+                    </Col>
+                    <Col xs={6}>
+                      {" "}
+                      <div>
+                        <h6>Quantidade:</h6>
+                        <span>abc@abc</span>
+                      </div>
+                    </Col>
+                    <Col xs={6}>
+                      {" "}
+                      <div>
+                        <h6>Código de Desconto:</h6>
+                        <span>99999999</span>
+                      </div>
+                    </Col>
+                    <Col xs={6}>
+                      <div>
+                        <h6>Preço Inicial:</h6>
+                        <span>abc</span>
+                      </div>
+                    </Col>
+                    <Col xs={6}>
+                      <div>
+                        <h6>Desconto (Manual):</h6>
+                        <span>abc</span>
+                      </div>
+                    </Col>
+                    <Col xs={6}>
+                      <div>
+                        <h6>Estimativa Preço Final:</h6>
+                        <span>abc</span>
+                      </div>
+                    </Col>
+                  </Row>
+                </BoxContainer>
+              </Col>
+            </Row>
+          </Container>
+        </Col>
+      </EncomendaWrapper>
+    </Container>
+  );
 }
+
+const EncomendaWrapper = styled(Row)`
+  border: 1px solid black;
+`;
+
+const ButtonsRow = styled(Row)``;
+
+const BoxContainer = styled(Container)`
+  border: 1px solid black;
+`;
+
+const BoxCol = styled(Col)`
+  border: 1px solid black;
+`;
